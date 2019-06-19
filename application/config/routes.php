928624469f93c216704ec41ea82952164e7a502d
Sigfrid <sigfridgom@gmail.com>
2019-06-19 09:45:33 -0600

    CAMBIOS

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'usuarioss';
+$route['default_controller'] = 'usuarios';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
