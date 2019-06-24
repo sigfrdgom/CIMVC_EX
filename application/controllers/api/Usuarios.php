11a7b16517743919f3cb3c3014afa7b41ab2c4b3
Sigfrid <sigfridgom@gmail.com>
2019-06-24 16:22:28 -0600

    rest

---
+++ application/controllers/api/Usuarios.php
@@ -0,0 +1,75 @@
+<?php
+   
+require APPPATH . 'libraries/REST_Controller.php';
+     
+class Usuarios extends REST_Controller {
+    
+	  /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function __construct() {
+       parent::__construct();
+       $this->load->database();
+    }
+       
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+	public function index_get($id = 0)
+	{
+        if(!empty($id)){
+            $data = $this->db->get_where("usuarios", ['id' => $id])->row_array();
+            if (count($data) == 0) {
+                $this->response(['Item not exist'], REST_Controller::HTTP_OK);
+                return;
+            }
+        }else{
+            $data = $this->db->get("usuarios")->result();
+        }
+     
+        $this->response($data, REST_Controller::HTTP_OK);
+	}
+      
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_post()
+    {
+        $input = $this->input->post();
+        $this->db->insert('usuarios',$input);
+     
+        $this->response(['Item created successfully.'], REST_Controller::HTTP_OK);
+    } 
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_put($id)
+    {
+        $input = $this->put();
+        $this->db->update('usuarios', $input, array('id'=>$id));
+     
+        $this->response(['Item updated successfully.'], REST_Controller::HTTP_OK);
+    }
+     
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_delete($id)
+    {
+        $this->db->delete('usuarios', array('id'=>$id));
+       
+        $this->response(['Item deleted successfully.'], REST_Controller::HTTP_OK);
+    }
+    	
+}
