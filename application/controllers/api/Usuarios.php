eb26474449603e605680d93a891bf2fa71af74e4
Sigfrid <sigfridgom@gmail.com>
2019-06-25 11:48:44 -0600

    API-REST TESTEADA!

--- application/controllers/api/Usuarios.php
+++ application/controllers/api/Usuarios.php
@@ -3,73 +3,52 @@
 require APPPATH . 'libraries/REST_Controller.php';
      
 class Usuarios extends REST_Controller {
-    
-	  /**
-     * Get All Data from this method.
-     *
-     * @return Response
-    */
+
     public function __construct() {
-       parent::__construct();
-       $this->load->database();
-    }
-       
-    /**
-     * Get All Data from this method.
-     *
-     * @return Response
-    */
-	public function index_get($id = 0)
-	{
+        parent::__construct();
+        $this->load->database();
+     }
+
+    // METHOD GET
+    public function index_get($id = 0)
+    {
         if(!empty($id)){
             $data = $this->db->get_where("usuarios", ['id' => $id])->row_array();
             if (count($data) == 0) {
-                $this->response(['Item not exist'], REST_Controller::HTTP_OK);
+                $this->response(["El usuario con id $id no existe"], REST_Controller::HTTP_OK);
                 return;
             }
         }else{
             $data = $this->db->get("usuarios")->result();
         }
-     
         $this->response($data, REST_Controller::HTTP_OK);
-	}
-      
-    /**
-     * Get All Data from this method.
-     *
-     * @return Response
-    */
+    }
+
+    // METHOD POST 
     public function index_post()
-    {
-        $input = $this->input->post();
-        $this->db->insert('usuarios',$input);
-     
-        $this->response(['Item created successfully.'], REST_Controller::HTTP_OK);
-    } 
-     
-    /**
-     * Get All Data from this method.
-     *
-     * @return Response
-    */
-    public function index_put($id)
-    {
-        $input = $this->put();
-        $this->db->update('usuarios', $input, array('id'=>$id));
-     
-        $this->response(['Item updated successfully.'], REST_Controller::HTTP_OK);
-    }
-     
-    /**
-     * Get All Data from this method.
-     *
-     * @return Response
-    */
-    public function index_delete($id)
-    {
-        $this->db->delete('usuarios', array('id'=>$id));
-       
-        $this->response(['Item deleted successfully.'], REST_Controller::HTTP_OK);
-    }
-    	
+     {
+         $input = $this->input->post();
+         $this->db->insert('usuarios',$input);
+      
+         $this->response(['El usuario ha sido creado!'], REST_Controller::HTTP_OK);
+     } 
+
+    // METHOD PUT
+     public function index_put($id)
+     {
+         $input = $this->put();
+         $this->db->update('usuarios', $input, array('id'=>$id));
+      
+         $this->response(['El usuario ha actualizado!'], REST_Controller::HTTP_OK);
+     }
+
+     // METHOD DELETE
+     public function index_delete($id)
+     {
+         $this->db->delete('usuarios', array('id'=>$id));
+        
+         $this->response(["Usuario  con id $id eliminado!"], REST_Controller::HTTP_OK);
+     }
+
 }
+?>
