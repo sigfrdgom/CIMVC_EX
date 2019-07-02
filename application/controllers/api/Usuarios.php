722970b87a8f4886c349697bff94874c662a2604
Sigfrid <sigfridgom@gmail.com>
2019-07-02 08:21:07 -0600

    minimos_cambios

--- application/controllers/api/Usuarios.php
+++ application/controllers/api/Usuarios.php
@@ -5,6 +5,8 @@ require APPPATH . 'libraries/REST_Controller.php';
 class Usuarios extends REST_Controller {
 
     public function __construct() {
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         parent::__construct();
         $this->load->database();
      }
